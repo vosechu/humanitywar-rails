28254c7e2f47d9810934b68bb21fe8dcebf955e5
phBits <phBits@server>
2013-03-20 12:21:33 -0700

    added whitecard specs and fixed other specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ HumanitywarRails::Application.routes.draw do
   resources :playas
 
 
-  match '/'   => 'entries#index', :via => :get
+  match 'entries/'   => 'entries#index', :via => :get
   match 'entries/win'   => 'entries#win', :via => :put
   match 'entries/draw'  => 'entries#draw', :via => :put
   match 'entries/?'     => 'entries#index', :via => :get
