4de481331bbea11aebdf6e1804fe956de4038307
Chuck Lauer Vose <vosechu@gmail.com>
2013-03-30 12:55:08 -0700

    change schema to use email

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(:version => 20130320181058) do
   end
 
   create_table "playas", :force => true do |t|
-    t.string   "user"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
